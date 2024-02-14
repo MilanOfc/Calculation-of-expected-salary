de609ad0a2bcc8b555215c5279526abb9e9e8acd
MilanOfc <antsyrev@gmail.com>
2024-02-14 14:38:44 +0100

    split the function for HH and SJ into two separate functions

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from itertools import count
 
 HH_URL = 'https://api.hh.ru/vacancies/'
 SJ_URL = 'https://api.superjob.ru/2.0/vacancies/'
+SJ_MOSCOW_ID = 4
+SJ_INDUSTRY_ID = 48
+HH_MOSCOW_ID = 1
+HH_DAYS_PERIOD = 30
 LANG_NAMES = [
     'JavaScript',
     'Java',
@@ -15,33 +19,39 @@ LANG_NAMES = [
     'C++',
     'C#',
     'Go',
-    'Shell'
-    ]
+    'Shell',
+]
 COLUMN_NAMES = [
     "Язык программирования",
     "Вакансий найдено",
     "Вакансий обработано",
-    "Средняя зарплата"
-    ]
+    "Средняя зарплата",
+]
 
 
-def fetch_records(url, params, amount_kw, items_kw, headers=None):
-    if headers is None:
-        headers = {}
+def fetch_records_SJ(url, params, headers):
     for page in count():
         params['page'] = page
         page_response = requests.get(url, headers=headers, params=params)
         page_response.raise_for_status()
         page_payload = page_response.json()
-        yield from page_payload[items_kw]
-        if page_payload.get('pages'):
-            if page >= page_payload['pages'] or page >= 99:  # number of records per page - 20,
-                                                             # hh maximum allows you to dowload out 2000 records
-                                                             # that's why "page >= 99"
-                yield page_payload[amount_kw]
+        yield from page_payload['objects']
         if page_payload.get('more') == False:  # Cannot write "if not page_payload.get('more')", cause
             # "not None = True" but "None != False"
-            yield page_payload[amount_kw]
+            yield page_payload['total']
+
+
+def fetch_records_HH(url, params):
+    for page in count():
+        params['page'] = page
+        page_response = requests.get(url, params=params)
+        page_response.raise_for_status()
+        page_payload = page_response.json()
+        yield from page_payload['items']
+        if page >= page_payload['pages'] or page >= 99:  # number of records per page - 20,
+            # hh maximum allows you to download out 2000 records
+            # that's why "page >= 99"
+            yield page_payload['found']
 
 
 def predict_salary(salary_from, salary_to):
@@ -54,9 +64,8 @@ def predict_salary(salary_from, salary_to):
 
 def predict_rub_salary_hh(vacancy):
     salary = vacancy.get('salary')
-    if salary:
-        if salary['currency'] == 'RUR':
-            return predict_salary(salary['from'], salary['to']) or None
+    if salary and salary['currency'] == 'RUR':
+        return predict_salary(salary['from'], salary['to']) or None
 
 
 def predict_rub_salary_sj(vacancy):
@@ -64,43 +73,62 @@ def predict_rub_salary_sj(vacancy):
         return predict_salary(vacancy['payment_from'], vacancy['payment_to']) or None
 
 
-def make_stat_per_lang_for_hh_and_sj(url, headers, params, amount_kw, items_kw, sleep_timer, salary_func):
+def make_stat_per_lang_for_hh(url, params, salary_func):
     vacancies_stats_per_lang = {}
     for lang_name in LANG_NAMES:
         salaries = []
-        if params.get('text'):
-            params['text'] = f"программист {lang_name}"
-        else:
-            params['keyword'] = lang_name
-        for vacancy in fetch_records(url,
-                                     params,
-                                     headers=headers,
-                                     amount_kw=amount_kw,
-                                     items_kw=items_kw,
-                                     ):
+        params['text'] = f"программист {lang_name}"
+        for vacancy in fetch_records_HH(url, params):
             if isinstance(vacancy, int):
                 amount_of_vacancies = vacancy
                 break
             salaries.append(salary_func(vacancy))
         salaries = [salary for salary in salaries if salary]
-        temp = {
+        vacancies_stats_for_lang = {
             "vacancies_found": amount_of_vacancies,
             "vacancies_processed": len(salaries),
         }
-        if len(salaries) == 0:
-            temp["average_salary"] = 0
+        if not salaries:
+            vacancies_stats_for_lang["average_salary"] = 0
         else:
-            temp["average_salary"] = int(sum(salaries) / len(salaries))
-        vacancies_stats_per_lang[lang_name] = temp
-        time.sleep(sleep_timer)  # without sleeptimer hh asks for CAPTCHA after 120 requests
+            vacancies_stats_for_lang["average_salary"] = int(sum(salaries) / len(salaries))
+        vacancies_stats_per_lang[lang_name] = vacancies_stats_for_lang
+        time.sleep(30)  # without sleeptimer hh asks for CAPTCHA after 120 requests
     return vacancies_stats_per_lang
 
 
-def make_table(vacancies_information, platform_name):
+def make_stat_per_lang_for_sj(url, headers, params, salary_func):
+    vacancies_stats_per_lang = {}
+    for lang_name in LANG_NAMES:
+        salaries = []
+        params['keyword'] = lang_name
+        for vacancy in fetch_records_SJ(url, params, headers):
+            if isinstance(vacancy, int):
+                amount_of_vacancies = vacancy
+                break
+            salaries.append(salary_func(vacancy))
+        salaries = [salary for salary in salaries if salary]
+        vacancies_stats_for_lang = {
+            "vacancies_found": amount_of_vacancies,
+            "vacancies_processed": len(salaries),
+        }
+        if not salaries:
+            vacancies_stats_for_lang["average_salary"] = 0
+        else:
+            vacancies_stats_for_lang["average_salary"] = int(sum(salaries) / len(salaries))
+        vacancies_stats_per_lang[lang_name] = vacancies_stats_for_lang
+    return vacancies_stats_per_lang
+
+
+def make_table(vacancies_statistics, platform_name):
     title = platform_name
     table_data = [COLUMN_NAMES]
-    for key, value in vacancies_information.items():
-        table_data.append([key, value["vacancies_found"], value["vacancies_processed"], value["average_salary"]])
+    for lang_name, lang_statistics in vacancies_statistics.items():
+        table_data.append([lang_name,
+                           lang_statistics["vacancies_found"],
+                           lang_statistics["vacancies_processed"],
+                           lang_statistics["average_salary"],
+                           ])
     return AsciiTable(table_data, title).table
 
 
@@ -112,24 +140,17 @@ if __name__ == '__main__':
     sj_arguments = {
         'url': SJ_URL,
         'headers': sj_headers,
-        'params': {'town': 4, 'catalogues': 48},
-        'amount_kw': 'total',
-        'items_kw': 'objects',
-        'sleep_timer': 0,
+        'params': {'town': SJ_MOSCOW_ID, 'catalogues': SJ_INDUSTRY_ID},
         'salary_func': predict_rub_salary_sj,
     }
     hh_arguments = {
         'url': HH_URL,
-        'headers': None,
-        'params': {'text': True, 'area': 1, 'period': 30},
-        'amount_kw': 'found',
-        'items_kw': 'items',
-        'sleep_timer': 30,
+        'params': {'text': True, 'area': HH_MOSCOW_ID, 'period': HH_DAYS_PERIOD},
         'salary_func': predict_rub_salary_hh,
     }
 
-    hh_stats = make_stat_per_lang_for_hh_and_sj(**hh_arguments)
-    sj_stats = make_stat_per_lang_for_hh_and_sj(**sj_arguments)
+    sj_stats = make_stat_per_lang_for_sj(**sj_arguments)
+    hh_stats = make_stat_per_lang_for_hh(**hh_arguments)
     print(make_table(sj_stats, 'SuperJob Moscow'))
     print()
     print(make_table(hh_stats, 'HeadHunter Moscow'))
